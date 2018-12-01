24cefeb1e7219cf9830c463bb895611046c7291c
Smushed <smushedcode@gmail.com>
2018-12-01 12:51:58 -0600

    Started the paths working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,3 @@
-var express = require("express");
-var path = require("path");
+module.exports = function (app) {
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
+};
