52792654d4e461147c60cb1b29fec02a847ac8be
Smushed <smushedcode@gmail.com>
2018-12-01 22:37:43 -0600

    get and post working in JS, now need to build the friend finder functionality and modals

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,12 @@
-const friendData = require(`../data/friends`);
+const friendArray = require(`../data/friends`);
 
 module.exports = function (app) {
-    app.get("api/friends", (req, res) => {
-        res.json(friendData);
+    app.get(`/api/friends`, (req, res) => {
+        res.json(friendArray);
+    });
+
+    app.post(`/api/friends`, (req, res) => {
+        friendArray.push(req.body);
+        res.json(true);
     });
 };
